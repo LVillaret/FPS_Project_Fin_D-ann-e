8af1dff6baeba3047394b91d3b7e644e4bdf95ec
Logan Villaret <l.villaret@ensi.lan>
2026-03-26 11:54:16 +0100

    dégat,shoot,kill,movement,colision

--- Assets/Scripts/Raycast.cs
+++ Assets/Scripts/Raycast.cs
@@ -5,21 +5,31 @@ using UnityEngine;
 public class Raycast : MonoBehaviour
 {
     [SerializeField] private Transform _originTransform;
+    public GameObject _fire;
+    public GameObject _hitPoint;
 
+    private void Update()
+    {
+        Shooting();
+    }
 
-    private void Update() {
+    public void Shooting()
+    {
         Debug.DrawRay(_originTransform.position, _originTransform.forward * 100, Color.yellow);
-        if (Input.GetButtonDown("Fire2")) {
+        if (Input.GetButtonDown("Fire1")) {
             Ray ray = new Ray(_originTransform.position, _originTransform.forward);
 
             if (Physics.Raycast(ray, out RaycastHit hit)){
                 TargetController target = hit.transform.GetComponent<TargetController>();
                 if (target != null) target.TakeDamage(1);
             }
+            GameObject a = Instantiate(_fire, _originTransform.position, Quaternion.identity);
+            GameObject b = Instantiate(_hitPoint, hit.point, Quaternion.identity);
+
+            Destroy(a, 0.5f);
+            Destroy(b, 2f);
         }
     }
-    
-       
 
 }
 
--- Assets/Scripts/TargetController.cs
+++ Assets/Scripts/TargetController.cs
@@ -4,11 +4,11 @@ public class TargetController : MonoBehaviour
 {
    [SerializeField] private float _currenteHealth;
 
-   public void TakeDamage(int damage) {
-       
+   public void TakeDamage(int damage) 
+   {
        _currenteHealth -= damage;
-       if (_currenteHealth <= 0) {
-           
+       if (_currenteHealth <= 0)
+       {
            Destroy(gameObject);
        }
    }
