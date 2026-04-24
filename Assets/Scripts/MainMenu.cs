71c58e25deac677cd24384ab831bc96413b43278
Logan Villaret <l.villaret@ensi.lan>
2026-04-24 13:27:54 +0200

    AddedMainMenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
